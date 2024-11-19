521608ff6827882524017c028635ef67f628c27f
Angela Feng <anfeng2@berkeley.edu>
2024-11-19 10:56:14 -0800

    add swift files

---
+++ glowgoal/glowgoalTests/glowgoalTests.swift
@@ -0,0 +1,17 @@
+//
+//  glowgoalTests.swift
+//  glowgoalTests
+//
+//  Created by Angela Feng on 11/19/24.
+//
+
+import Testing
+@testable import glowgoal
+
+struct glowgoalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
